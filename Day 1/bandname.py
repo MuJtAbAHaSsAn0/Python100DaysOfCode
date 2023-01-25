d3b32be018c88866abebc5d271e5aead010f5486
Mujtaba Hassan <hassanad1214@gmail.com>
2023-01-25 02:10:54 -0800

    Day 1

---
+++ Day 1/bandname.py
@@ -0,0 +1,5 @@
+print("Hello, do you wanna create your band name ?")
+name = input("If yes, What is your name ?")
+pet = input("what is your pet name ?")
+print("Your Band name is" + name,pet)
+#print(len(name))
